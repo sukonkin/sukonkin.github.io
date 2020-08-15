0ad044fe5a09827c4d4e66531a1b735e73e4db11
Sergey Sukonkin <69344353+sukonkin@users.noreply.github.com>
2020-08-15 18:12:38 +0300

    Заглушка
    Поставил зашлущку, вместо предыдущей версии сайта.

---
+++ js/script.js
@@ -0,0 +1,25 @@
+var welcome = [
+"<h1>Привет!</h1>",
+"<h1>Вітання!</h1>",
+"<h1>Salam!</h1>",
+"<h1>Përshëndetje!</h1>",
+"<h1>Hello!</h1>",
+"<h1>Բարեւ Ձեզ!</h1>",
+"<h1>Hallo!</h1>",
+"<h1>Kaixo!</h1>",
+"<h1>Прывітанне!</h1>",
+"<h1>გამარჯობა!</h1>",
+"<h1>Hej!</h1>",
+"<h1>Sawubona!</h1>",
+"<h1>Dia dhuit!</h1>",
+"<h1>Halló!</h1>",
+"<h1>여보세요!</h1>",
+"<h1>Sveiki!</h1>",
+"<h1>Dzień dobry!</h1>",
+"<h1>Olá!</h1>",
+"<h1>Ahoj!</h1>",
+"<h1>Zdravo!</h1>",
+"<h1>Hallå!</h1>"
+];
+
+document.write(welcome[Math.floor(Math.random() * welcome.length)]);
