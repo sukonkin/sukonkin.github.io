aea25bc512fc4fdfd7d07cda3571c4b1852084a5
Sergey Sukonkin <69344353+sukonkin@users.noreply.github.com>
2020-08-26 03:13:44 +0300

    Изменен JS
    Изменен JavaScript-код. Изменение коснулось вывода на страницу приветствий на разных языках.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,26 @@
 var welcome = [
-"<h1>Привет!</h1>",
-"<h1>Вітання!</h1>",
-"<h1>Salam!</h1>",
-"<h1>Përshëndetje!</h1>",
-"<h1>Hello!</h1>",
-"<h1>Բարեւ Ձեզ!</h1>",
-"<h1>Hallo!</h1>",
-"<h1>Kaixo!</h1>",
-"<h1>Прывітанне!</h1>",
-"<h1>გამარჯობა!</h1>",
-"<h1>Hej!</h1>",
-"<h1>Sawubona!</h1>",
-"<h1>Dia dhuit!</h1>",
-"<h1>Halló!</h1>",
-"<h1>여보세요!</h1>",
-"<h1>Sveiki!</h1>",
-"<h1>Dzień dobry!</h1>",
-"<h1>Olá!</h1>",
-"<h1>Ahoj!</h1>",
-"<h1>Zdravo!</h1>",
-"<h1>Hallå!</h1>"
+"Привет!",
+"Вітання!",
+"Salam!",
+"Përshëndetje!",
+"Hello!",
+"Բարեւ Ձեզ!",
+"Hallo!",
+"Kaixo!",
+"Прывітанне!",
+"გამარჯობა!",
+"Hej!",
+"Sawubona!",
+"Dia dhuit!",
+"Halló!",
+"여보세요!",
+"Sveiki!",
+"Dzień dobry!",
+"Olá!",
+"Ahoj!",
+"Zdravo!",
+"Hallå!"
 ];
 
-document.write(welcome[Math.floor(Math.random() * welcome.length)]);
+var el = document.getElementById("greeting");
+el.textContent = welcome[Math.floor(Math.random() * welcome.length)];
